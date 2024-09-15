214dbe2f59f4e205f54685440b768a45401ed771
Joy Bhuiya <joybhuiya36@gmail.com>
2024-09-15 14:08:30 +0600

    feat: Implemented Mutation endpoints and hooks for mutating data

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,19 +1,55 @@
 import { Button, Checkbox, Flex, Input, Spin, Typography } from "antd";
-import { useGetTasksQuery } from "../../services/apliSlice";
+import {
+  useAddTaskMutation,
+  useDeleteTaskMutation,
+  useGetTasksQuery,
+  useUpdateTaskMutation,
+} from "../../services/apliSlice";
 import "./Home.scss";
 import { useState } from "react";
 
 const Home = () => {
   const [text, setText] = useState("");
-  const { data: tasksList, isLoading, isSuccess, isError } = useGetTasksQuery();
+  const {
+    data: tasksList,
+    isLoading,
+    isSuccess,
+    isError,
+    refetch,
+  } = useGetTasksQuery();
+  const [addTask] = useAddTaskMutation();
+  const [updateTask] = useUpdateTaskMutation();
+  const [deleteTask] = useDeleteTaskMutation();
+
   const { Text } = Typography;
+
+  const handleAddClick = () => {
+    if (text.trim() != "") {
+      const taskData = JSON.parse(JSON.stringify(tasksList));
+      const data = {
+        id: (Number(taskData[0]?.id) + 1).toString(),
+        title: text.trim(),
+        isCompleted: false,
+      };
+      addTask(data);
+    }
+    setText("");
+  };
+
+  const handleOnDelete = (id) => (e) => {
+    deleteTask(id);
+  };
 
   const handleInputOnChange = (e) => {
     const value = e.target.value;
     setText(value);
   };
-  const handleCheckboxOnChange = (e) => {
+
+  const handleCheckboxOnChange = (id) => (e) => {
     const isChecked = e.target.checked;
+    let updatedTask = { ...tasksList?.find((task) => task?.id == id) };
+    updatedTask.isCompleted = isChecked;
+    updateTask(updatedTask);
   };
 
   return (
@@ -21,24 +57,29 @@ const Home = () => {
       <Text className="task__header">My Tasks</Text>
       <Flex className="task__inputWarapper">
         <Input value={text} onChange={handleInputOnChange} />
-        <Button>Add</Button>
+        <Button onClick={handleAddClick}>Add</Button>
       </Flex>
       <Flex vertical gap={10}>
         {isLoading && <Spin size="large" />}
         {isError && <Text className="task__error">Something went wrong</Text>}
-        {tasksList?.map((task, index) => (
-          <Flex key={index}>
+        {tasksList &&
+          tasksList?.map((task, index) => (
             <Checkbox
+              key={index}
               checked={task?.isCompleted}
-              onChange={handleCheckboxOnChange}
+              onChange={handleCheckboxOnChange(task?.id)}
             >
               <Flex justify="space-between" className="task__item">
-                <Text>{task?.title}</Text>
-                <Button className="task__button">X</Button>
+                <Text style={{ textAlign: "left" }}>{task?.title}</Text>
+                <Button
+                  className="task__button"
+                  onClick={handleOnDelete(task?.id)}
+                >
+                  X
+                </Button>
               </Flex>
             </Checkbox>
-          </Flex>
-        ))}
+          ))}
       </Flex>
     </Flex>
   );
--- src/services/apliSlice.js
+++ src/services/apliSlice.js
@@ -4,11 +4,45 @@ const baseurl = import.meta.env.VITE_BASE_URL;
 
 export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: baseurl }),
+  tagTypes: ["Tasks"],
   endpoints: (builder) => ({
     getTasks: builder.query({
       query: () => "/tasks",
+      transformResponse: (tasks) => tasks.reverse(),
+      providesTags: ["Tasks"],
+    }),
+
+    addTask: builder.mutation({
+      query: (task) => ({
+        url: "/tasks",
+        method: "POST",
+        body: task,
+      }),
+      invalidatesTags: ["Tasks"],
+    }),
+
+    updateTask: builder.mutation({
+      query: ({ id, ...updatedTask }) => ({
+        url: `/tasks/${id}`,
+        method: "PATCH",
+        body: updatedTask,
+      }),
+      invalidatesTags: ["Tasks"],
+    }),
+
+    deleteTask: builder.mutation({
+      query: (id) => ({
+        url: `/tasks/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["Tasks"],
     }),
   }),
 });
 
-export const { useGetTasksQuery } = api;
+export const {
+  useGetTasksQuery,
+  useAddTaskMutation,
+  useUpdateTaskMutation,
+  useDeleteTaskMutation,
+} = api;
